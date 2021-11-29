b706bf695967ae59caf2c1c3ac797f9400aefda2
attachemd <mattache.vivace@gmail.com>
2021-11-29 17:16:22 +0100

    Code Updated.

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -3,10 +3,11 @@ import {MatButtonModule} from "@angular/material/button";
 import {MatIconModule} from "@angular/material/icon";
 import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatInputModule} from "@angular/material/input";
+import {FlexLayoutModule} from "@angular/flex-layout";
 
 @NgModule({
-  imports: [MatButtonModule, MatIconModule, MatInputModule, MatFormFieldModule],
-  exports: [MatButtonModule, MatIconModule, MatInputModule, MatFormFieldModule]
+  imports: [MatButtonModule, MatIconModule, MatInputModule, MatFormFieldModule, FlexLayoutModule],
+  exports: [MatButtonModule, MatIconModule, MatInputModule, MatFormFieldModule, FlexLayoutModule]
 })
 export class MaterialModule {
 
