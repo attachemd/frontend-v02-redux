aea67bb07d73283d1e4f2cfb21621380a8b81325
attachemd <mattache.vivace@gmail.com>
2021-12-02 10:45:30 +0100

    Passing data to the dialog.

--- src/app/training/current-training/current-training.component.ts
+++ src/app/training/current-training/current-training.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {MatDialog} from "@angular/material/dialog";
-import { StopTrainingComponent } from './stop-training.component';
+import {StopTrainingComponent} from './stop-training.component';
 
 @Component({
     selector: 'app-current-training',
@@ -17,7 +17,7 @@ export class CurrentTrainingComponent implements OnInit {
     ngOnInit(): void {
         this.timer = setInterval(() => {
             this.progress += 5;
-            if(this.progress >= 100){
+            if (this.progress >= 100) {
                 clearInterval(this.timer);
             }
         }, 1000)
@@ -25,7 +25,17 @@ export class CurrentTrainingComponent implements OnInit {
 
     onStop(): void {
         clearInterval(this.timer);
-        this.dialog.open(StopTrainingComponent);
+        const dialog = this.dialog.open(
+            StopTrainingComponent, {
+                data: {
+                    progress: this.progress
+                }
+            }
+        );
+        dialog.afterClosed().subscribe(result => {
+            console.log("result: ");
+            console.log(result);
+        })
     }
 
 }
